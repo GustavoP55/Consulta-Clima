e0ff87b053e86e5d0f4bb0c293e4430da43cbec0
Gustavo <gustavoporfirio0326@gmail.com>
2026-08-08 15:19:59 -0300

    Primeira versão funcional

---
+++ src/js/script.js
@@ -0,0 +1,58 @@
+document.querySelector('#search').addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const cityName = document.querySelector('#cityName').value;
+
+    if(!cityName){
+        document.querySelector("#weather").classList.remove('show')
+        showAlert('Digite uma cidade válida');
+        return;
+    }
+
+    const apiKey = '';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(cityName)}&appid=${apiKey}&units=metric&lang=pt_br`;
+
+    const result = await fetch(apiUrl);
+    const json = await result.json();
+
+    if(json.cod === 200){
+        showInfos({
+            city: json.name,
+            country: json.sys.country,
+            temp: json.main.temp,
+            tempMax: json.main.temp_max,
+            tempMin: json.main.temp_min,
+            description: json.weather[0].description,
+            tempIcon: json.weather[0].icon,
+            windSpeed: json.wind.speed,
+            humidity: json.main.humidity,
+        });
+    }
+    else{
+        document.querySelector("#weather").classList.remove('show')
+        showAlert(
+            `Não foi possível encontrar...
+
+            <img src='src/images/404.svg'/>
+        `)
+    }
+})
+
+function showInfos(json){
+    showAlert('');
+
+    document.querySelector("#weather").classList.add('show')
+    document.querySelector('#titulo').innerHTML = `${json.city}, ${json.country}`;
+    document.querySelector('#tempValue').innerHTML = `${json.temp.toFixed(1).toString().replace('.', ',')} <sup>C°</sup>`;
+    document.querySelector('#tempImage').setAttribute('src', `https://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
+
+    document.querySelector('#tempMax').innerHTML = `${json.tempMax.toFixed(1).toString().replace('.', ',')} <sup>C°</sup>`;
+    document.querySelector('#tempMin').innerHTML = `${json.tempMin.toFixed(1).toString().replace('.', ',')} <sup>C°</sup>`;
+    document.querySelector('#humidity').innerHTML = `${json.humidity}%`;
+    document.querySelector('#wind').innerHTML = `${json.windSpeed.toFixed(1)}km/h`;
+
+}
+
+function showAlert(msg){
+    document.querySelector('#alert').innerHTML = msg;
+}
